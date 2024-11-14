11a48b6ea234eabb3c4425a6ec242974795ec8e8
orion <asiskrich@gmail.com>
2024-11-14 12:01:40 +0300

    colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,28 @@
 return {
+  -- {
+  --   "sainnhe/sonokai",
+  --   priority = 500,
+  --   config = function()
+  --     vim.g.sonokai_transparent_background = "1"
+  --     vim.g.sonokai_enable_italic = "1"
+  --     vim.g.sonokai_style = "andromeda"
+  --     vim.cmd.colorscheme("sonokai")
+  --   end,
+  -- },
   {
-    "sainnhe/sonokai",
+    "maxmx03/fluoromachine.nvim",
+    lazy = false,
     priority = 1000,
     config = function()
-      vim.g.sonokai_transparent_background = "1"
-      vim.g.sonokai_enable_italic = "1"
-      vim.g.sonokai_style = "andromeda"
-      vim.cmd.colorscheme("sonokai")
+      local fm = require("fluoromachine")
+
+      fm.setup({
+        glow = true,
+        theme = "fluoromachine",
+        transparent = true,
+      })
+
+      vim.cmd.colorscheme("fluoromachine")
     end,
   },
 }
